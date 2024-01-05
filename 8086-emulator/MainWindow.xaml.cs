8ddb675fea8625bae1909d04b89938524a470250
Konrad Marciniak <konrad.marciniak@microsoft.wsei.edu.pl>
2024-01-05 16:40:49 +0100

    registers -random, reset and insert added

--- 8086-emulator/MainWindow.xaml.cs
+++ 8086-emulator/MainWindow.xaml.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Automation;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
@@ -37,15 +39,85 @@ namespace _8086_emulator
             {
                 Trace.WriteLine($"Instruction: {comboBoxInstruction.Text}");
                 Trace.WriteLine($"From: {comboBoxRegister1.Text} \tTo: {comboBoxRegister2.Text}");
-            } else
+            }
+            else
             {
                 Trace.WriteLine($"Can't {comboBoxInstruction.Text} from {comboBoxRegister1.Text} to {comboBoxRegister2.Text}.");
                 Trace.Write("Please choose different registers");
             }
-
-            // We want a RESET and RANDOM button
-            // We also want ACTUAL registers
             // And the ability to write to them.
+        }
+
+        private void HandleInput(string input, TextBlock register)
+        {
+            string hexPattern = "^[0-9a-fA-F]+$";
+
+            if (Regex.IsMatch(input, hexPattern))
+            {
+                // valid input, assign it to register.Text
+                register.Text = input.PadLeft(4, '0');
+            }
+            else
+            {
+                // input not valid -> display an error message
+                MessageBox.Show("Invalid input. Please enter a valid hexadecimal value.");
+            }
+        }
+
+        private void InsertManualValueAX(object sender, RoutedEventArgs e)
+        {
+            HandleInput(registerAX_Input.Text, registerAX);
+        }
+
+        private void InsertManualValueBX(object sender, RoutedEventArgs e)
+        {
+            HandleInput(registerBX_Input.Text, registerBX);
+        }
+
+        private void InsertManualValueCX(object sender, RoutedEventArgs e)
+        {
+            HandleInput(registerCX_Input.Text, registerCX);
+        }
+
+        private void InsertManualValueDX(object sender, RoutedEventArgs e)
+        {
+            HandleInput(registerDX_Input.Text, registerDX);
+        }
+
+        private void RandomRegisterValues(object sender, RoutedEventArgs e)
+        {
+            // Randomize register values
+
+            Random random = new Random();
+
+            List<TextBlock> registers = new List<TextBlock>
+            {
+                registerAX,
+                registerBX,
+                registerCX,
+                registerDX
+            };
+
+            // for each register
+
+            foreach (TextBlock block in registers)
+            {
+                byte byte1 = (byte)random.Next(256);
+                byte byte2 = (byte)random.Next(256);
+
+                ushort randomValue = (ushort)((byte2 << 8) | byte1);
+
+                block.Text = randomValue.ToString("X4");
+
+            }
+        }
+        private void ResetRegisterValues(object sender, RoutedEventArgs e)
+        {
+            // Reset
+            registerAX.Text = "0000";
+            registerBX.Text = "0000";
+            registerCX.Text = "0000";
+            registerDX.Text = "0000";
         }
     }
 }
